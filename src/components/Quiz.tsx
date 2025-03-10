f4bc6362797753a8f5cb6fb73954d98d0843e958
nadiia-dev <nadiia.pavliuchenko.dev@gmail.com>
2025-03-10 11:37:18 +0200

    add styles to quiz component

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -13,13 +13,19 @@ const Quiz = () => {
   };
 
   return (
-    <div>
-      <div>
-        <h2>{questions[currentQuestion].text}</h2>
-        <ul>
+    <div className="bg-indigo-950 w-full max-w-lg lg:max-w-3xl flex items-center justify-center p-4 rounded-lg shadow-lg">
+      <div className="p-4 max-w-lg text-center">
+        <h2 className="text-sky-50 text-sm">
+          {questions[currentQuestion].text}
+        </h2>
+        <ul className="mt-4 space-y-3">
           {questions[currentQuestion].answers.map((answer, index) => (
             <li key={index}>
-              <button type="button" onClick={() => selectAnswer(answer)}>
+              <button
+                type="button"
+                onClick={() => selectAnswer(answer)}
+                className="w-full bg-sky-500 text-white text-md py-2 px-10 rounded-full transition-all duration-300 hover:bg-purple-500 focus:bg-purple-500 sm:px-12 md:px-14 lg:px-16"
+              >
                 {answer}
               </button>
             </li>
