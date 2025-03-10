b8a52043f2909eacc475370bf8659439cb530d48
nadiia-dev <nadiia.pavliuchenko.dev@gmail.com>
2025-03-10 10:53:21 +0200

    init project

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <>Main</>;
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), svgr(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      src: "/src",
+      assets: "/src/assets",
+    },
+  },
+  base: "/",
+});
